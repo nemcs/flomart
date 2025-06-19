57b985a342705afdd1346d2301d66c40304ce789
Vitaly <vitkamenev08@gmail.com>
2025-06-19 12:23:57 +0300

    init apperror

--- internal/apperror/error.go
+++ internal/apperror/error.go
@@ -1,6 +1,8 @@
 package apperror
 
-import "fmt"
+import (
+	"fmt"
+)
 
 type AppError struct {
 	UserMsg string
@@ -11,9 +13,30 @@ type AppError struct {
 
 func (e *AppError) Error() string {
 	if e.Err != nil {
-		return fmt.Sprintf("%s: %s", e.DevMsg, e.Err.Error())
+		return fmt.Sprintf("%s : %s", e.DevMsg, e.Err.Error())
 	}
 	return e.DevMsg
+}
+
+// Позволяет использовать errors.Is() и errors.As() с AppError, чтобы доставать вложенные ошибки.
+func (e *AppError) Unwrap() error {
+	return e.Err
+}
+func New(err error, userMsg, devMsg string, code int) *AppError {
+	return &AppError{
+		UserMsg: userMsg,
+		DevMsg:  devMsg,
+		Err:     err,
+		Code:    code,
+	}
+}
+func Wrap(err error, userMsg, devMsg string, code int) *AppError {
+	return &AppError{
+		UserMsg: userMsg,
+		DevMsg:  devMsg,
+		Err:     err,
+		Code:    code,
+	}
 }
 
 /*
